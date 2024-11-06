358579fdc0b39783b1793413274c092b056e9e18
vadim <vadim.lylko@gmail.com>
2024-11-06 23:59:41 +0200

    add empty ProfessionCard.vue, ProfessionsListSection.vue

---
+++ src/components/layout/ProfessionCard.vue
@@ -0,0 +1,13 @@
+<script setup>
+
+</script>
+
+<template>
+<li class="profession-item">
+
+</li>
+</template>
+
+<style scoped lang="scss">
+
+</style>
---
+++ src/components/layout/ProfessionsListSection.vue
@@ -0,0 +1,19 @@
+<script setup>
+
+import ProfessionCard from "@/components/layout/ProfessionCard.vue";
+</script>
+
+<template>
+<section class="section professions-list">
+  <div class="professions-list__container container">
+    <h2 class="heading-2 professions-list__title">Усі професії</h2>
+    <ul class="professions-list__list">
+      <profession-card/>
+    </ul>
+  </div>
+</section>
+</template>
+
+<style scoped lang="scss">
+
+</style>
