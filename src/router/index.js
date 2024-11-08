6fa31988bc307eaa393a9cf226783e3b6f9985b0
vadim <vadim.lylko@gmail.com>
2024-11-08 19:51:59 +0200

    add scrollBehavior to the routing

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,13 @@
 import {createRouter, createWebHistory} from "vue-router";
 const router = createRouter({
     history: createWebHistory(),
+    scrollBehavior(to, from, savedPosition) {
+        if (savedPosition) {
+            return savedPosition;
+        } else {
+            return { top: 0 };
+        }
+    },
     routes: [
         {
             name: "home",
