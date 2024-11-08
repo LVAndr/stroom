4594235ade0cb0c32955a717c3516ad4d9aab1ce
vadim <vadim.lylko@gmail.com>
2024-11-08 18:43:22 +0200

    Updates

--- assets/User-DvCn9rs9.js
+++
@@ -1,1 +0,0 @@
-import{_ as o,c as t,a as s,o as c}from"./index-Dxpg02c6.js";const n={},r={class:"section"};function a(_,e){return c(),t("section",r,e[0]||(e[0]=[s("div",{class:"container"},[s("h1",null,"Сторінка в розробці")],-1)]))}const l=o(n,[["render",a]]);export{l as default};
